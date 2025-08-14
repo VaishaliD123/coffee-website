16041e74f12dddc6d34557c659b05322189a7d9e
Your Name <vaishdhanasekar2003@gmail.com>
2025-08-14 13:19:08 +0530

    Initial commit - Coffee Website

---
+++ README.md
@@ -0,0 +1,12 @@
+# Coffee-Website
+This is a coffee website which is developed with PHP
+
+# Technologies used
+- HTML
+- CSS
+- JavaScript
+- PHP
+- MySQL
+
+# License
+This project is under <a href="https://github.com/ValentineFernandes/Coffee-Website/blob/main/LICENSE"> MIT </a> license.
---
+++ about.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Best coffee in Chennai">
+    <meta name="author" content="">
+
+    <title>The Perfect Cup - About Us</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/business-casual.css" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+
+    <style>
+        .popular-coffee img {
+            width: 250px;
+            height: 250px;
+            object-fit: cover;
+            margin: 0 auto;
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <div class="brand">The Perfect Cup - Chennai</div>
+    <div class="address-bar">123 Marina Street | Chennai, TN 600001 | +91 98765 43210</div>
+
+    <!-- Navigation -->
+    <?php require_once 'nav.php'; ?>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">About
+                        <strong>The Perfect Cup - Chennai</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-md-6">
+                    <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style>
+                    <div class='embed-container'>
+                        <iframe src='https://www.youtube.com/embed/ooUAVHJAcyk' frameborder='0' allowfullscreen></iframe>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <p>The Perfect Cup - Chennai opened its doors in 2010 near Marina Beach. Our mission is simple: to serve Chennai’s coffee lovers with the finest brews in town.
+                    <br><br>
+                    Our baristas are passionate about coffee and trained to bring out the best in every bean. We combine traditional brewing techniques with local flavours to create a coffee experience like no other.</p>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="box popular-coffee">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Our
+                        <strong>Popular Coffee</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-responsive" src="https://surl.lt/cytpva" alt="Latte Coffee">
+                    <h3>Latte Coffee</h3>
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-responsive" src="https://surl.li/aswvhb" alt="Cold Brew">
+                    <h3>Cold Brew</h3>
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-responsive" src="https://surl.lu/njnhat" alt="Muffin Coffee">
+                    <h3>Muffin Coffee</h3>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; The Perfect Cup Chennai 2025</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
